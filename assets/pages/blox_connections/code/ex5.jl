ed2dad82d9cb53e1258da9e681b0e14faf024b0c
harisorgn <organtzh@gmail.com>
2025-01-10 08:30:29 +0000

    Deploying to gh-pages from  @ ae4b66b6b6fb97ddd679c35421d770819b6bdc88 🚀

--- assets/pages/blox_connections/code/ex5.jl
+++ assets/pages/blox_connections/code/ex5.jl
@@ -3,12 +3,8 @@ struct IzhNeuron <: Neuron
     system
     namespace
 
-    # We keep all function arguments as keyword arguments
-    # so that we can set them more conveniently as `arg = value`.
     function IzhNeuron(; name, namespace=nothing, a=0.02, b=0.2, V_reset=-50, d=2, threshold=30)
         sts = @variables V(t)=-65 [output=true] u(t)=-13 jcn [input=true]
-        # Spike threshold `θ=30` is now included as a parameter.
-        # Default values for all parameters are the keyword arguments from above. This way we can set them easily during construction.
         params = @parameters a=a b=b V_reset=V_reset d=d θ=threshold
 
         eqs = [D(V) ~ 0.04 * V ^ 2 + 5 * V + 140 - u + jcn + 5,
@@ -20,3 +16,5 @@ struct IzhNeuron <: Neuron
         new(sys, namespace)
     end
 end
+
+# In the `IzhNeuron` constructor function we keep all arguments as keyword arguments so that we can set them more conveniently as `arg = value`. Spike threshold `θ=30` is now included as a parameter. Default values for all parameters are the keyword arguments from above. This way we can set them easily during construction.
